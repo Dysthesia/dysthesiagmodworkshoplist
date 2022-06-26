dfe4c232004051819fe2de71501460db7beb2e7f
Dysthesia <77560444+Dysthesia@users.noreply.github.com>
2022-06-27 07:34:03 +0800

    Add files via upload

---
+++ workshop.lua
@@ -0,0 +1,52 @@
+-- GMOD Workshop List by Dysthesia
+
+-- Placeholder:
+	resource.AddWorkshop()
+
+-- GAMEMODES:
+
+	-- PropHunt (Hide'n'Seek) - Original
+		resource.AddWorkshop(135509255)
+	-- Murder
+		resource.AddWorkshop(187073946)
+	-- You Touched it Last
+		resource.AddWorkshop(447829341)
+	-- Cops and Runners
+		resource.AddWorkshop(277013349)
+	-- Melonbomber
+		resource.AddWorkshop(237537750)
+
+
+-- MAP:
+
+-- WEAPON:
+
+	-- M9K Specialties
+		144982052)
+
+	-- M9K Small Arms pack
+		128093075
+
+-- VEHICLE:
+
+
+-- NPC:
+
+	-- VJ Base
+		131759821)
+
+-- TOOL:
+
+	-- Addon Share
+		973145750)
+
+
+-- ENTITY:
+-- EFFECTS:
+-- MODEL:
+
+	-- Genshin Impact Character Pack - PMs and NPCs
+		resource.AddWorkshop(252304893)
+
+
+-- SERVERCONTENT:
