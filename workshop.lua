e1cf9473d43291d1693f9d9b1a6be562cf11b70a
Dysthesia <77560444+Dysthesia@users.noreply.github.com>
2022-06-27 09:08:29 +0800

    Add files via upload

--- workshop.lua
+++ workshop.lua
@@ -1,11 +1,25 @@
 -- GMOD Workshop List by Dysthesia
 
+--[[
+Format:
+
+	-- Addon Name
+		resource.workshop(workshopnumber)
+
+workshopnumber = 2002082140
+Located at the end of the workshop link:
+https://steamcommunity.com/sharedfiles/filedetails/?id=2002082140
+--]]
+
 -- Placeholder:
 	resource.AddWorkshop()
 
+print("Thank you for using GMOD Workshop List by Dysthesia!")
+print("Version 6.27.22")
+
 -- GAMEMODES:
 
-	-- PropHunt (Hide'n'Seek) - Original
+	-- PropHunt Hide'n'Seek - Original
 		resource.AddWorkshop(135509255)
 	-- Murder
 		resource.AddWorkshop(187073946)
@@ -15,30 +29,296 @@
 		resource.AddWorkshop(277013349)
 	-- Melonbomber
 		resource.AddWorkshop(237537750)
-
-
--- MAP:
-
+	-- Zombie Survival
+		resource.AddWorkshop(105462463)
+	-- Hide and Seek
+		resource.AddWorkshop(266512527)
+	-- Guess Who
+		resource.AddWorkshop(480998235)
+	-- Overwatch
+		resource.AddWorkshop(2713876035)
+	-- Trash Compactor Gamemode
+		resource.AddWorkshop(495998201)
+	-- Extreme Football Throwdown
+		resource.AddWorkshop(104625632)
+	-- Attack on Titan Gamemode Alpha v1.1
+		resource.AddWorkshop(237201758)
+	-- nZombies Unlimited - Content Pack
+		resource.AddWorkshop(1712782641)
+	-- nZombies Unlimited [v1.0]
+		resource.AddWorkshop(1737426708)
+		
+-- MAPS:
+--[[
+	"Maps are optional to include as the server automatically downloads maps 
+	regardless of whether it is included in the workshop.lua file or not. If however for some reason the game didn't download the map from the client, add it here in the maps section.
+		- Dysthesia
+--]]		
+		
 -- WEAPON:
 
 	-- M9K Specialties
-		144982052)
-
+		resource.AddWorkshop(144982052)
 	-- M9K Small Arms pack
-		128093075
-
+		resource.AddWorkshop(128093075)
+	-- Star Wars Lightsabers
+		resource.AddWorkshop(111412589)
+	-- Aperture Science Handheld Portal Device
+		resource.AddWorkshop(336509414)
+	-- Minecraft SWEPs
+		resource.AddWorkshop(116592647)
+	-- CS:GO Knives SWEPs
+		resource.AddWorkshop(506283460)
+	-- FA:S 2.0 Alpha SWEPs
+		resource.AddWorkshop(180507408)
+	-- FA:S 2.0 Alpha SWEPs - Pistols
+		resource.AddWorkshop(181283903)
+	-- FA:S 2.0 Alpha SWEPs - Rifles
+		resource.AddWorkshop(181656972)
+	-- FA:S 2.0 Alpha SWEPs - SMG
+		resource.AddWorkshop(183139624)
+	-- FA:S 2.0 Alpha SWEPs - Shotguns
+		resource.AddWorkshop(183140076)
+	-- FA:S 2.0 Alpha SWEPs - Misc
+		resource.AddWorkshop(201027186)
+	-- FA:S 2.0 Alpha SWEPs - U. Rifles
+		resource.AddWorkshop(201027715)
+	-- MHs Super Battle Pack PART ONE
+		resource.AddWorkshop(362207690)
+	-- MH's Super Battle Pack PART II
+		resource.AddWorkshop(270907600)
+	-- Russian Roulette Weapon
+		resource.AddWorkshop(175874148)
+	-- [ArcCW] Gunsmith Offensive
+		resource.AddWorkshop(2257255110)
+	-- [ArcCW] Arctic's Customizable Weapons Base Only
+		resource.AddWorkshop(2131057232)
+	-- [ArcCW] Modern Warfare 2
+		resource.AddWorkshop(2135529088)
+	-- Wheatley's Parkour
+		resource.AddWorkshop(410645074)
+	-- TFA Base [ Reduxed ]
+		resource.AddWorkshop(415143062)
+	-- [TFA][AT]MWR Snipers Pt.1
+		resource.AddWorkshop(1474344971)
+	-- [TFA][AT]MWR Snipers Pt.2
+		resource.AddWorkshop(1474349017)
+	-- [TFA][AT]MWR SMG Pack 1
+		resource.AddWorkshop(1463436406)
+	-- [TFA][AT]MWR SMG Pack 2
+		resource.AddWorkshop(1468627718)
+	-- [TFA][AT]MWR Shotguns
+		resource.AddWorkshop(1458818112)
+	-- [TFA][AT]MWR Shared Pack
+		resource.AddWorkshop(1458801852)
+	-- [TFA][AT]MWR Rifles Pt.1
+		resource.AddWorkshop(1475907552)
+	-- [TFA][AT]MWR Rifles Pt.2
+		resource.AddWorkshop(1475907935)
+	-- [TFA][AT]MWR Rifles Pt.3
+		resource.AddWorkshop(1475912539)
+	-- [TFA][AT]MWR Pistols
+		resource.AddWorkshop(1458804593)
+	-- [TFA][AT]MWR LMGs Pt.1
+		resource.AddWorkshop(1472622234)
+	-- [TFA][AT]MWR LMGs Pt.2
+		resource.AddWorkshop(1472622182)
+		
 -- VEHICLE:
 
+	-- Parakeet's Pill Pack
+		resource.AddWorkshop(950845673)
+	-- Minecraft Pill Pack
+		resource.AddWorkshop(325599907)
+	-- SCars Basic
+		resource.AddWorkshop(104487316)
+	-- WAC Aircraft
+		resource.AddWorkshop(104990330)
+	-- SCars Slim
+		resource.AddWorkshop(104483020)
+	-- SCars Extra	
+		resource.AddWorkshop(104492363)
+	-- [simfphys] LUA Vehicles - Base
+		resource.AddWorkshop(771487490)
+	-- [simfphys] GTA San Andreas Cars
+		resource.AddWorkshop(1737050476)
+	-- [simfphys] armed vehicles
+		resource.AddWorkshop(831680603)
+	-- TDMCars - Base Pack
+		resource.AddWorkshop(112606459)
+	-- TDMSCars
+		resource.AddWorkshop(112607905)
+	-- TDMCars - Chevrolet
+		resource.AddWorkshop(113997239)
+	-- TDMCars - Audi
+		resource.AddWorkshop(113120185)
+	-- BMV
+		resource.AddWorkshop(113118541)
+	-- Ferrari
+		resource.AddWorkshop(119148996)
+	-- 
+	-- TDMCars - 
 
 -- NPC:
 
 	-- VJ Base
-		131759821)
-
+		resource.AddWorkshop(131759821)
+	-- Zombie SNPCs
+		resource.AddWorkshop(152529683)
+	-- Obunga Nextbot
+		resource.AddWorkshop(2823271127)
+	-- Chasing Shrek Nextbot
+		resource.AddWorkshop(519446152)
+	-- Amogus Chaser
+		resource.AddWorkshop(2431689413)
+	-- Quandale Dingle Nextbot
+		resource.AddWorkshop(2794401258)
+	-- Pedobear Nextbots
+		resource.AddWorkshop(624583385)
+	-- DrGBase Nextbot Base
+		resource.AddWorkshop(1560118657)
+	-- Micheal Myers
+		resource.AddWorkshop(1301120069)
+	-- Yui Kimura
+		resource.AddWorkshop(2036186484)
+	-- Kazan Yamaoka The Oni
+		resource.AddWorkshop(1933506902)
+	-- Kate Denson Additional
+		resource.AddWorkshop(1892859856)
+	-- Nancy Wheeler
+		resource.AddWorkshop(1875614467)
+	-- Steve Harrington
+		resource.AddWorkshop(1854851583)
+	-- Demogorgon
+		resource.AddWorkshop(1864152569)
+	-- Ash J. Williams
+		resource.AddWorkshop(1778357787)
+	-- Adris The Plague
+		resource.AddWorkshop(1739505790)
+	-- Jane Romero
+		resource.AddWorkshop(1697857071)
+	-- The Legion
+		resource.AddWorkshop(1587095021)
+	-- [VJ] Borderlands 2 SNPCs V2.0
+		resource.AddWorkshop(1633068592)
+	-- Jeff Johansen
+		resource.AddWorkshop(1579683750)
+	-- The Doctor
+		resource.AddWorkshop(1554143989)
+	-- Adam Francis
+		resource.AddWorkshop(1520459302)
+	-- The Huntress
+		resource.AddWorkshop(1467693157)
+	-- Kate Denson Default
+		resource.AddWorkshop(1435050148)
+	-- Leon Kennedy
+		resource.AddWorkshop(1428613972)
+	-- Jill Valentine Pirate
+		resource.AddWorkshop(1415906089)
+	-- Ace Visconti
+		resource.AddWorkshop(1411054383)
+	-- Bill Overbeck
+		resource.AddWorkshop(1411049450)
+	-- Feng Min
+		resource.AddWorkshop(1391907422)
+	-- David King
+		resource.AddWorkshop(1387377849)
+	-- David Tapp
+		resource.AddWorkshop(1311556144)
+	-- Nea Karlsson
+		resource.AddWorkshop(1308437884)
+	-- Laurie Strode
+		resource.AddWorkshop(1284795344)
+	-- The Pig
+		resource.AddWorkshop(1286160451)
+	-- Freddy Kruger
+		resource.AddWorkshop(1278485876)
+	-- Quentin Smith
+		resource.AddWorkshop(1268892570)
+	-- Claudette Morel
+		resource.AddWorkshop(1263656636)
+	-- Meg Thomas
+		resource.AddWorkshop(1260682890)
+	-- Trapper
+		resource.AddWorkshop(1198024651)
+	-- Nextbot Myers
+		resource.AddWorkshop(2455728323)
+	-- Onryo Nextbot
+		resource.AddWorkshop(2790987177)
+	-- Artist Nextbot
+		resource.AddWorkshop(2729757216)
+	-- Cenobite Nextbot
+		resource.AddWorkshop(2616986792)
+	-- Nextbot Nemesis
+		resource.AddWorkshop(2527546156)
+	-- Nextbot Trickster
+		resource.AddWorkshop(2446934818)
+	-- Nextbot Twins
+		resource.AddWorkshop(2514213468)
+	-- Nextbot Blight
+		resource.AddWorkshop(2498856923)
+	-- Nextbot PyraHead
+		resource.AddWorkshop(2458447139)
+	-- Nextbot Deathslinger
+		resource.AddWorkshop(2454331799)
+	-- Nextbot Oni
+		resource.AddWorkshop(2484325242)
+	-- Nextbot Demogorgon
+		resource.AddWorkshop(2470507648)
+	-- Nextbot Ghostface
+		resource.AddWorkshop(2427010120)
+	-- Nextbot Plague
+		resource.AddWorkshop(2449157596)
+	-- Nextbot Legion
+		resource.AddWorkshop(2432206363)
+	-- Nextbot Spirit
+		resource.AddWorkshop(2429851282)
+	-- Nextbot Clown
+		resource.AddWorkshop(2435866336)		
+	-- Nextbot Pig
+		resource.AddWorkshop(2428021681)
+	-- Nextbot Nightmare
+		resource.AddWorkshop(2465121189)
+	-- Nextbot Cannibal
+		resource.AddWorkshop(2432795601)
+	-- Nextbot Huntress
+		resource.AddWorkshop(2440936960)
+	-- Nextbot Doctor
+		resource.AddWorkshop(2493024497)
+	-- Nextbot Hag
+		resource.AddWorkshop(2476142974)		
+	-- Nextbot Nurse
+		resource.AddWorkshop(2504107724)
+	-- Nextbot Hillbilly
+		resource.AddWorkshop(2424287823)
+	-- Nextbot Wraith
+		resource.AddWorkshop(2421056427)	
+	-- Nextbot trapper
+		resource.AddWorkshop(2419684337)
+	-- DBD SNPCs Alt cos
+		resource.AddWorkshop(2277287076) 
+	-- DBD SNPC PACK ASSETS
+		resource.AddWorkshop(1945196363)
+	-- DBD SNPCS PACK
+		(1941874996)
+	--
+		
 -- TOOL:
 
 	-- Addon Share
-		973145750)
+		resource.AddWorkshop(973145750)
+	-- Hit Numbers
+		resource.AddWorkshop(110871780)
+	-- Outfitter
+		resource.AddWorkshop(882463775)
+	-- More Materials!
+		resource.AddWorkshop(105841291)
+	-- Enhanced Playermodel Selector
+		resource.AddWorkshop(504945881)
+	-- 3D2D Textscreens
+		resource.AddWorkshop(109643223)
+	-- Simple ThirdPerson
+		resource.AddWorkshop(207948202)
 
 
 -- ENTITY:
@@ -47,6 +327,49 @@
 
 	-- Genshin Impact Character Pack - PMs and NPCs
 		resource.AddWorkshop(252304893)
-
-
+	-- Genshin Impact Character Outfit Alternatives - PMs and NPCs
+		resource.AddWorkshop(2514139627)
+	-- [PM] PaiMAN | Genshin Impact
+		resource.AddWorkshop(2566096656)
+	-- Valorant - Phoenix PM+NPC
+		resource.AddWorkshop(2082172806)
+	-- JETT [PLAYERMODEL] // VALORANT
+		resource.AddWorkshop(2061938989)
+	-- Dead by Daylight - Leatherface PM+NPC
+		resource.AddWorkshop(1171508304)
+	-- Gmod Legs 3
+		resource.AddWorkshop(112806637)
+	-- GTA 5 Playermodels
+		resource.AddWorkshop(216408135)
+	-- GMod Tower: Player Model Pack
+		resource.AddWorkshop(150404359)
+	-- NieR: Automata 2B ENHANCED V2 [PM/NPC]
+		resource.AddWorkshop(905397141)
+	-- DBD Sign Props
+		resource.AddWorkshop(2400465292)
+	-- DBD Autohaven Props
+		resource.AddWorkshop(2399217125)
+	-- DBD MacMillian Props
+		resource.AddWorkshop(2366653438)
+	-- DBD halloween props
+		resource.AddWorkshop(2231283339)
+		
+		
+		
 -- SERVERCONTENT:
+
+
+	-- Player Status Icons
+		resource.AddWorkshop(2002082140)
+	-- Freddy Fazbear's Pizza Map Content
+		resource.AddWorkshop(2599447131)
+	-- ULib
+		resource.AddWorkshop(557962238)
+	-- ULX
+		resource.AddWorkshop(557962280)
+	-- Custom Chat
+		resource.AddWorkshop(279930710)
+	-- Ultimate Workshop Downloader
+		resource.AddWorkshop(2214712098)
+
+
